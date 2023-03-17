a8a387ebdd7da1c2cb8f457995e1236b256a030e
Alimagdi96 <aliabdaldaim1234@gmail.com>
2023-03-17 13:37:05 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *puts function
+ *descriptin: of c programing
+ *return: value = 0 mean sucsess
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
