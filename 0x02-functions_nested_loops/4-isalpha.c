3728c84352cee0e6e5cebefab19b43771736be78
Alimagdi96 <aliabdaldaim1234@gmail.com>
2023-03-21 17:46:41 -0700

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -18,36 +18,3 @@ int _isalpha(int c)
 		return (0);
 	}
 }
-
-
-Task 5
-vi 5-sign.c
-
-# include "main.h"
-/**
- * print_sign-check the sign of a number.
- *
- * @n:Number whose sign is to be checked.
- * Return:1-if n is greater than 0: 0- if n equals 0:-1-if n is less than 0
- */
-
-
-
-int print_sign(int n)
-{
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar ('-');
-		return (-1);
-	}
-	else
-	{
-		_putchar('0');
-		return (0);
-	}
-}
